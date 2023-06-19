a22b35b4169bfd00e2a20d18b1f35354d1051e17
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-06-19 12:23:28 +0530

    Create Q3.js

---
+++ Q3.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const Todo = require('../models/todo');
+
+router.get('/api/todo/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await Todo.findById(id);
+    if (!todo) {
+      return res.status(404).json({ message: 'Todo not found' });
+    }
+    res.json(todo);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
